43ef53c38dab322d307164d6be5e9aa28f0c7ee3
EronRodrigoF <eronrodrigof@yahoo.com.br>
2021-08-04 18:06:08 -0300

    client product

---
+++ src/main/java/com/eronrodrigo/productcatalog/model/Product.java
@@ -0,0 +1,32 @@
+package com.eronrodrigo.productcatalog.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor 
+@AllArgsConstructor
+@Getter 
+@Setter
+@Entity
+@Table(name = "PRODUCTS")
+public class Product {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+
+}
